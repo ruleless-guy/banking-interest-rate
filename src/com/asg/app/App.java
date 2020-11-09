fe7fd65a8a6126afe9e03f184aef0cb44dc929ef
ruleless-guy <khun.dev.git@gmail.com>
2020-11-09 21:12:48 +0630

    first commit

---
+++ src/com/asg/app/App.java
@@ -0,0 +1,18 @@
+package com.asg.app;
+
+public class App {
+	public static void main(String[] args) {
+		App app = new App();
+		System.out.println(app.calculate(50, 5, 12));
+	}
+	
+	public double calculate(double deposits,double interestRate,int month) {
+		double total = 0;
+		float rate = (float) (interestRate / 100);
+		for(int i = 1; i <= month; i++) {
+			total = (total + deposits) * (1 + rate);
+			
+		}
+		return total;
+	}
+}
